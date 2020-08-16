964b1878d63b657f61d3dae5bbe71b69fecba1ae
Yousof Ebneddin <yousofebneddin@yahoo.com>
2020-08-16 12:17:55 -0700

    set relative RPATH to make the install directory, movable.

--- CMake/install_config.cmake
+++ CMake/install_config.cmake
@@ -1,6 +1,8 @@
 # Set CMAKE_INSTALL_* if not defined
 set(CMAKECONFIG_INSTALL_DIR "${CMAKE_INSTALL_LIBDIR}/cmake/${LRS_TARGET}")
 
+set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_RPATH}:\$ORIGIN:\$ORIGIN/../lib:\$ORIGIN/../include")
+
 add_custom_target(uninstall "${CMAKE_COMMAND}" -P "${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake")
 
 include(CMakePackageConfigHelpers)
